772ce873a14e51a2b7a94421a28a8d1ed4f35630
alfreddai <ya95123@gmail.com>
2024-08-27 11:27:11 +0800

    feat: add click scroll & click open

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
+const bg1 = document.getElementById("bg-1");
+const bannerBtn = document.querySelector(".banner-btn");
+const openBtn = document.querySelector(".open-btn");
+const openContent = document.querySelector(".open-content");
 const lenis = new Lenis();
 
-lenis.on("scroll", (e) => {
-  console.log(e);
-});
-
-function raf(time) {
+const raf = (time) => {
   lenis.raf(time);
   requestAnimationFrame(raf);
-}
+};
 
 requestAnimationFrame(raf);
+
+// lenis.on("scroll", (e) => {
+//   console.log(e);
+// });
+
+bannerBtn.addEventListener("click", () => {
+  lenis.scrollTo(bg1, {
+    duration: 1.8,
+  });
+});
+
+openBtn.addEventListener("click", () => {
+  openContent.style.height = "auto";
+});
